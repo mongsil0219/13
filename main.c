e7cdc363c10df6333cc7176ba577ca4e5c5dcdeb
mongsil0219 <113421366+mongsil0219@users.noreply.github.com>
2022-11-29 15:58:52 +0900

    2

--- main.c
+++ main.c
@@ -1,28 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-#define MAX_NAME  20
-//구조체 선언  
-struct student {
-		int 	ID;
-		char 	name[MAX_NAME];
-		double 	grade;
-	};
+struct point{
+	int x;
+	int y;
+};
 
-int main(int argc, char *argv[]) {
-	//intstance
-	struct student student1 = {123456, "Juyeop", 4.3};
+int main (void){
+	struct point p1, p2;
+	int xdiff, ydiff;
+	double dist;
 	
-	//구조체값 초기화  
-	student1.ID = 1004;
-	strcpy(student1.name, "Kim");
-	student1.grade = 3.3;
+	printf("input p1 coordinate(x y) :");
+	scanf("%d %d", &p1.x, &p1.y);
 	
-	printf("ID : %i\n", student1.ID);
-	printf("name : %s\n", student1.name);
-	printf("grade : %f\n", student1.grade);
+	printf("input p2 coordinate(x y) :");
+	scanf("%d %d", &p2.x, &p2.y);
 	
+	xdiff = p2.x - p1.x;
+	ydiff = p2.y - p1.y;
 	
-	return 0;
+	dist = sqrt(xdiff*xdiff + ydiff*ydiff);
+	
+	printf("distance : %f\n", dist);
+	
 }
